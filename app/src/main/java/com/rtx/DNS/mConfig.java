f7dc08b983e511c5dbfe2cf6c34875b2a9ecb0bd
ronnie240486 <ronnie240486@gmail.com>
2026-09-17 01:27:57 -0300

    Add files via upload

--- app/src/main/java/com/rtx/DNS/mConfig.java
+++ app/src/main/java/com/rtx/DNS/mConfig.java
@@ -2,6 +2,6 @@ package com.rtx.DNS;
 
 /* JADX INFO: loaded from: classes2.dex */
 public class mConfig {
-    public static String mAPI = "https://renciaapp-ldyffp73.manus.space/api/";
+    public static String mAPI = "https://renciaapp-production.up.railway.app/api/";
     public static int mRefreshInteval = 1;
 }
